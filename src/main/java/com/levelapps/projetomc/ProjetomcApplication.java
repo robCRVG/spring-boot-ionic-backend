79a65b588eb96a960e7da9b92877ca3e634a577e
robCRVG_ <robjr2013@gmail.com>
2021-02-13 20:05:02 -0300

    Criando instanciação

--- src/main/java/com/levelapps/projetomc/ProjetomcApplication.java
+++ src/main/java/com/levelapps/projetomc/ProjetomcApplication.java
@@ -1,13 +1,33 @@
 package com.levelapps.projetomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetomcApplication {
+import com.levelapps.projetomc.domain.Categoria;
+import com.levelapps.projetomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria (null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
