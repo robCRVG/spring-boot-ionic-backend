0730702664ad923e0fef939388aa81aa7fcb80ee
robCRVG_ <robjr2013@gmail.com>
2021-02-13 20:44:20 -0300

    Assiciação muitos pra muitos Produto Categoria

--- src/main/java/com/levelapps/projetomc/ProjetomcApplication.java
+++ src/main/java/com/levelapps/projetomc/ProjetomcApplication.java
@@ -1,5 +1,6 @@
 package com.levelapps.projetomc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.levelapps.projetomc.domain.Categoria;
+import com.levelapps.projetomc.domain.Produto;
 import com.levelapps.projetomc.repositories.CategoriaRepository;
+import com.levelapps.projetomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjetomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
@@ -26,7 +32,19 @@ public class ProjetomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria (null, "Escritório");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 600.00);
+		Produto p3 = new Produto(null, "Mouse", 90.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,  cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
