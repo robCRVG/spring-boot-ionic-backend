20f8e6be47d9c3c2b7d84cce298b5f9d58721edb
robCRVG_ <robjr2013@gmail.com>
2021-02-13 18:02:16 -0300

    Testando classe domínio Categorias

--- src/main/java/com/levelapps/projetomc/resources/CategoriaResource.java
+++ src/main/java/com/levelapps/projetomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.levelapps.projetomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.levelapps.projetomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Test Rest funcionando";
+	public List<Categoria>listar() {
+		
+		Categoria categoria = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escrotório");
+		
+		
+		List<Categoria> listC = new ArrayList<Categoria>();
+		listC.add(categoria);
+		listC.add(categoria2);
+
+		return listC;
 	}
 
 }
