ad575143a0c70f91d753b74ac02562d11f872022
robCRVG_ <robjr2013@gmail.com>
2021-02-13 15:40:05 -0300

    Projeo criado

---
+++ src/main/java/com/levelapps/projetomc/ProjetomcApplication.java
@@ -0,0 +1,13 @@
+package com.levelapps.projetomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/levelapps/projetomc/ProjetomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelapps.projetomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
