c1d29cce847c9e096400883a8e63940240f4e0fc
robCRVG_ <robjr2013@gmail.com>
2021-02-14 15:49:56 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/levelapps/projetomc/resources/CategoriaResource.java
+++ src/main/java/com/levelapps/projetomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.levelapps.projetomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.levelapps.projetomc.domain.Categoria;
 import com.levelapps.projetomc.services.CategoriaService;
@@ -22,7 +26,13 @@ public class CategoriaResource {
 
 		Categoria categoria = categoriaService.buscar(id);
 		return ResponseEntity.ok().body(categoria);
-
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = categoriaService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("{id}").buildAndExpand(obj.getId()).toUri();		
+		return ResponseEntity.created(uri).build();
 	}
 
 }
