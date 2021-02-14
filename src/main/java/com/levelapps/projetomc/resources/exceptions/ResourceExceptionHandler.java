baacb17b2ba9a3ee95a4dea76850b4389554ec11
robCRVG_ <robjr2013@gmail.com>
2021-02-13 21:24:38 -0300

    Ajustes no endpoint /categorias/{id} - criação de exceção 404

---
+++ src/main/java/com/levelapps/projetomc/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.levelapps.projetomc.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.levelapps.projetomc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardErrors> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		StandardErrors errors = new StandardErrors(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errors);
+	}
+
+}
