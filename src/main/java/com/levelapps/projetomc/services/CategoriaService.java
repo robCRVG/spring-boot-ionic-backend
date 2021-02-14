d12bc803c389aeffb887b4a295874ad33f3e21c6
robCRVG_ <robjr2013@gmail.com>
2021-02-14 17:46:59 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/levelapps/projetomc/services/CategoriaService.java
+++ src/main/java/com/levelapps/projetomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.levelapps.projetomc.domain.Categoria;
@@ -40,12 +43,16 @@ public class CategoriaService {
 			categoriaRepository.deleteById(id);
 		}catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível exlcuir uma categoria que possui produto");
-		}
-				
+		}				
 	}
 
 	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 	
 }
