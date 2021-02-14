c14f9c99b2c4e8cc4ca931f27c485765ec08b73f
robCRVG_ <robjr2013@gmail.com>
2021-02-14 17:11:48 -0300

    Listar Categorias com DTO

--- src/main/java/com/levelapps/projetomc/services/CategoriaService.java
+++ src/main/java/com/levelapps/projetomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.levelapps.projetomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +44,8 @@ public class CategoriaService {
 				
 	}
 
+	public List<Categoria> findAll() {
+		return categoriaRepository.findAll();
+	}
 	
 }
