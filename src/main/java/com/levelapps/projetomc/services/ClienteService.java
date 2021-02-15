ce21afee9277eadf01da656d036c9258b4da30d6
robCRVG_ <robjr2013@gmail.com>
2021-02-15 00:09:34 -0300

    Ajustando deleção de Clientes

--- src/main/java/com/levelapps/projetomc/services/ClienteService.java
+++ src/main/java/com/levelapps/projetomc/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 		try {
 			clienteRepository.deleteById(id);
 		}catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível exlcuir uma cliente porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível exlcuir uma cliente porque há pedidos relacionadas");
 		}				
 	}
 
